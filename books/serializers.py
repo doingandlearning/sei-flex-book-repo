572a7d3c0b5fd3c4a05fe80c556a632f4c215638
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-04-23 16:54:24 +0100

    Removes unnecessary extra db dave.

--- books/serializers.py
+++ books/serializers.py
@@ -31,21 +31,19 @@ class BookSerializer(serializers.ModelSerializer):
         location_data = data.pop("locations")
 
         book = Book(**data)
-        # Need to save to get the id
-        book.save()
 
         if author_data:
             author, _created = Author.objects.get_or_create(**author_data)
             book.author = author
+
+        # Need to save to get the id
+        book.save()
 
         if location_data:
             for location in location_data:
                 newLocation, _created = Location.objects.get_or_create(
                     **location)
                 book.locations.add(newLocation)
-
-        # save to the database
-        book.save()
 
         # render to the api
         return book
